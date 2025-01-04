f8a20208c3cf1230024627a6a06f24c7d82e8c28
unhappyben <ben.renshaw@hotmail.com>
2025-01-04 14:48:21 +0100

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    hmr: {
-      overlay: true
-    }
-  }
-})
+    server: {
+      hmr: { overlay: true },
+    },
+  });
+};
