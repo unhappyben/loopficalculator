ff1013378680c0869c45cf585e8dd41c845f8c78
unhappyben <ben.renshaw@hotmail.com>
2025-01-04 15:06:01 +0100

    config oopsies

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 
-
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
     plugins: [react()],
     resolve: {
       alias: {
@@ -15,5 +14,5 @@ export default ({ mode }) => {
     server: {
       hmr: { overlay: true },
     },
-  });
-};
+  };
+});
