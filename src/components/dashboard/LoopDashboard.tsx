9e5510914bc07d0b19b894af113c66b10886c795
unhappyben <ben.renshaw@hotmail.com>
2025-01-04 15:15:32 +0100

    vercel analytics

--- src/components/dashboard/LoopDashboard.tsx
+++ src/components/dashboard/LoopDashboard.tsx
@@ -174,6 +174,8 @@ export default function LoopDashboard() {
       }
 
       return (
+      <>
+        <Analytics />  
         <div className="max-w-2xl mx-auto p-4">
           <Card>
             <CardHeader className="pb-4">
@@ -384,6 +386,7 @@ export default function LoopDashboard() {
         </CardContent>
       </Card>
     </div>
+    </>  
   );
 }
     
